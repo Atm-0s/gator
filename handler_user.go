de43c35812cf867f66bf42cee5625db4db6bfe11
Atm-0s <mikeymc@protonmail.com>
2025-11-11 11:06:08 +1100

    Lessons 1-3 from Ch.2 of the project

--- handler_user.go
+++ handler_user.go
@@ -1,23 +1,83 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/Atm-0s/BlogAggregator/internal/database"
+	"github.com/google/uuid"
 )
 
-func handlerLogin(s *state, cmd command) error {
+func argCheck(
+	cmd command,
+	tooFew string,
+) error {
 	if len(cmd.Args) == 0 {
-		return errors.New("login requires a username")
+		return errors.New(tooFew)
 	}
 	if len(cmd.Args) > 1 {
-		return errors.New("too many arguments provided, login expects one username")
+		return errors.New("too many arguments provided")
 	}
-	username := cmd.Args[0]
+	return nil
+}
 
-	err := s.cfgPtr.SetUser(username)
+func handlerLogin(s *state, cmd command) error {
+	err := argCheck(
+		cmd,
+		"login requires a username",
+	)
+	if err != nil {
+		return err
+	}
+
+	username := cmd.Args[0]
+	ctx := context.Background()
+	_, err = s.db.GetUser(ctx, username)
+	if err != nil {
+		return fmt.Errorf("user %s does not exist", username)
+	}
+
+	err = s.cfgPtr.SetUser(username)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 	fmt.Printf("User has been set to %s\n", username)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	err := argCheck(
+		cmd,
+		"register requires a name",
+	)
+	if err != nil {
+		return err
+	}
+	ctx := context.Background()
+	name := cmd.Args[0]
+	_, err = s.db.GetUser(ctx, name)
+	if err == nil {
+		return fmt.Errorf("user %s already exists", name)
+	}
+
+	userParams := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	}
+
+	newUser, err := s.db.CreateUser(ctx, userParams)
+	if err != nil {
+		return fmt.Errorf("error creating user: %w", err)
+	}
+	err = handlerLogin(s, cmd)
+	if err != nil {
+		return err
+	}
+	log.Printf("user was created: id=%s name=%s created_at=%s", newUser.ID, newUser.Name, newUser.CreatedAt)
 	return nil
 }
--- main.go
+++ main.go
@@ -1,29 +1,56 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
 	"github.com/Atm-0s/BlogAggregator/internal/config"
+	"github.com/Atm-0s/BlogAggregator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db     *database.Queries
 	cfgPtr *config.Config
 }
 
 func main() {
+	// Read config
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	// Open database
+	db, err := sql.Open("postgres", cfg.DbURL)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Initialise the database queries
+	dbQueries := database.New(db)
+
+	// Build state with config and queries
 	s := &state{
+		db:     dbQueries,
 		cfgPtr: &cfg,
 	}
+
+	// Register commands
 	c := commands{
 		cmdMap: make(map[string]func(*state, command) error),
 	}
 	c.register("login", handlerLogin)
+	c.register("register", handlerRegister)
 
+	// Parse input and run
 	input := os.Args
 	if len(input) < 2 {
 		fmt.Println("no arguments entered")
